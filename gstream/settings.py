127546e49a508b9e4ff56414f1ee3c9676e546be
Thomas Gruner <tom.gruner@gmail.com>
2014-06-02 16:23:46 -0700

    moved templates back into gstream dir

--- gstream/settings.py
+++ gstream/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+PROJECT_DIR = os.path.join(BASE_DIR, 'gstream')
 
 gettext = lambda s: s
 
@@ -141,7 +142,7 @@ MEDIA_URL = "/media/"
 SITE_ID = 1
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, "templates"),
+    os.path.join(PROJECT_DIR, "templates"),
 )
 
 CMS_TEMPLATES = (
